60ca1af01561d51333bcf6eccfdc031d4b012cae
Alexander Manley <alexander-manley@users.noreply.github.com>
2021-11-04 13:49:30 -0400

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
+
+import 'src/app.dart';
+
+void main() {
+  setHashUrlStrategy();
+  // setPathUrlStrategy();
+
+  runApp(const kubernetes-data-science-ui());
+}
